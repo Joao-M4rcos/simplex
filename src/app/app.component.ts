fe95c0e2f5e1bafa469bfe47611893e560024d55
João Marcos <joaomarcos@frwk.com>
2022-11-05 14:31:13 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'simplex';
+export class AppComponent implements OnInit {
+  simplexMaxForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.inicializarFormSimplexMax();
+  }
+
+  inicializarFormSimplexMax = () => {
+    this.simplexMaxForm = this.formBuilder.group({
+      Ax: [null],
+      Ay: [null],
+      AA: [null],
+      AB: [null],
+      AC: [null],
+      ARs: [null],
+      Bx: [null],
+      By: [null],
+      BA: [null],
+      BB: [null],
+      BC: [null],
+      BRs: [null],
+      Cx: [null],
+      Cy: [null],
+      CA: [null],
+      CB: [null],
+      CC: [null],
+      CRs: [null],
+      Zx: [null, [Validators.pattern('^[0-9]*$')]],
+      Zy: [null],
+      ZA: [null],
+      ZB: [null],
+      ZC: [null],
+      ZRs: [null],
+    });
+  };
+
+  identificarMenorElemento = () => {
+    const Zx = this.simplexMaxForm.get('Zx').value;
+    const Zy = this.simplexMaxForm.get('Zy').value;
+    // const ZA = this.simplexMaxForm.get('ZA').value;
+    // const ZB = this.simplexMaxForm.get('ZB').value;
+    // const ZC = this.simplexMaxForm.get('ZC').value;
+    // const ZRs = this.simplexMaxForm.get('ZRs').value;
+
+    let menorValor;
+
+    let elementos = [];
+
+    menorValor = Zx;
+
+    if (Zy < Zx) menorValor = Zy;
+    if (menorValor > 0) this.toastr.warning('O valor deve ser negativo!');
+    elementos.push(parseInt(Zy));
+    // elementos.push(ZA);
+    // elementos.push(ZB);
+    // elementos.push(ZC);
+
+    console.log(menorValor);
+  };
+
+  calcular = () => {
+    this.identificarMenorElemento();
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastrModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
